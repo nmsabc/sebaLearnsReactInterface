9665c492c41abcad969afd006e5dbd4b4dd11b60
Sebastian Ivan <sebastian.ivan@gmail.com>
2017-08-14 11:35:57 +0300

    Revert "Merge pull request #1 from nmsabc/hideAddAppointmentComponent"
    This reverts commit cae4dcd07e915dd6a20b9023eeb56a3d986a185e.

--- process/js/AddNewAppointment.js
+++ process/js/AddNewAppointment.js
@@ -28,31 +28,31 @@ AddNewAppointmentInDb: function(e){
 
     return(
       <div className="panel panel-primary">
-        <div className="panel-heading apt-addheading" onClick={this.toggleDisplayAddApointmentForm}>
+        <div className="panel-heading apt-addheading">
         <span className="glyphicon glyphicon-plus"></span> Add Appointment</div>
         <div className="panel-body" style={displayAddApointmentBody}>
           <form className="add-appointment form-horizontal" onSubmit = {this.AddNewAppointmentInDb}>
             <div className="form-group">
-            <label className="col-sm-2 control-label" htmlFor="petName">Pet Name</label>
+              <label className="col-sm-2 control-label" for="petName">Pet Name</label>
               <div className="col-sm-10">
                 <input type="text" className="form-control"
                   id="petName" ref="inputPetName" placeholder="Pet's Name" />
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label" htmlFor="petOwner">Pet Owner</label>
+              <label className="col-sm-2 control-label" for="petOwner">Pet Owner</label>
               <div className="col-sm-10">
                 <input type="text" className="form-control"
                   id="petOwner" ref="inputOwnerName" placeholder="Owner's Name" />
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label" htmlFor="aptDate">Date</label>
+              <label className="col-sm-2 control-label" for="aptDate">Date</label>
               <div className="col-sm-4">
                 <input type="date" className="form-control"
                   id="aptDate" ref="inputAptDate"/>
               </div>
-              <label className="col-sm-2 control-label" htmlFor="aptTime">Time</label>
+              <label className="col-sm-2 control-label" for="aptTime">Time</label>
               <div className="col-sm-4">
                 <input type="time" className="form-control"
                   id="aptTime" ref="inputAptTime" />
@@ -69,7 +69,7 @@ AddNewAppointmentInDb: function(e){
             </div>
 
             <div className="form-group">
-              <label className="col-sm-2 control-label" htmlFor="aptNotes">Apt. Notes</label>
+              <label className="col-sm-2 control-label" for="aptNotes">Apt. Notes</label>
               <div className="col-sm-10">
                 <textarea className="form-control" rows="4" cols="50"
                   id="aptNotes" ref="inputAptNotes" placeholder="Appointment Notes"></textarea>
